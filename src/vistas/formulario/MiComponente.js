a99be075ca77b2b8a4bad0a46626123476e40c27
Luis Fuentealba Pérez <44037835+lfuentealbap@users.noreply.github.com>
2021-12-06 18:30:30 -0300

    Todo funcionando

--- src/vistas/formulario/MiComponente.js
+++ src/vistas/formulario/MiComponente.js
@@ -6,12 +6,13 @@ import Swal from 'sweetalert2'
 const MiComponente = () => {
     const [patente, setPatente] = useState("")
     const [anio, setAnio] = useState("")
+    const [marca1, setMarca1] = useState("")
     const [marca, setMarca] = useState([])
     const [autos, setAutos] = useState([])
 
 
     const isMobile = useMediaQuery({ query: `(max-width: 760px)`});
-
+    
     const handleInputChangePatente = (event) => {
         setPatente(event.target.value)
     }
@@ -20,13 +21,14 @@ const MiComponente = () => {
         setAnio(event.target.value)
     }
     const cambioSelectMarca = (event) =>{
-        setMarca(event.target.value)
+        setMarca1(event.target.value)
     }
-
+    
     const enviarDatos = () => {
         console.log(`Enviando datos Patente:${patente} y Año:${anio}`)
 
         guardarAuto();
+        handleInputClean();
     }
 
     useEffect(() => {
@@ -62,7 +64,7 @@ const MiComponente = () => {
         axios.post('http://localhost:4000/api/autos', {
             patente: patente,
             anio: anio,
-            marca: marca
+            idMarca: marca1
         })
         .then(function (response){
             if(response.status == 200){
@@ -85,7 +87,7 @@ const MiComponente = () => {
     }
 
     
-
+/*
     const columns = [
         {
             name: "Marca",
@@ -114,68 +116,70 @@ const MiComponente = () => {
         
     ];
 
-    /*
+    
     const data1 = [
         {name: "Name 1", title: "Title 1", location: "Location 1", age: 30, salary: 10},
         {name: "Name 2", title: "Title 2", location: "Location 2", age: 31, salary: 11},
     ];
     */
     
-
+/*
     const handleRowClick = (rowData, rowMeta) => {
         console.log(rowData.name);
         setPatente(rowData.patente);
         setAnio(rowData.anio);
         setMarca(rowData.marca)
     };
-
+*/
     const handleInputClean = () => {
         setPatente('');
         setAnio('');
-        setMarca('');
+        setMarca1('');
     }
-
+/*
     const options = {
         filterType: 'checkbox',
         onlyOneRowCanBeSelected: true,
         onRowClick: handleRowClick
     };
-
+*/
     return (
         <body>
-            <div></div>
+            <div><br></br></div>
             <div className="container border">
                 <h3>Registro de autos</h3>
                 <br></br>
                 <br></br>
-                <div className="mb-3 col-3">
-                    <label for="patente" className="form-label">Patente</label>
-                    <input type="text" className="form-control" id="patente" ></input>
-                </div>
-                <div className="mb-3 col-3">
-                    <label for="anio" className="form-label">Año</label>
-                    <input type="text" className="form-control" id="anio"></input>
-                </div>
-                <div className="mb-3 col-3">
-                    <label for="marca" className="form-label">Marca</label>
-                    <select class="form-select" aria-label="Default select example" id="marca">
-                        <option selected>Seleccione una marca...</option>
-                        {
-                           marca?.map((marc, index)=>(
-                                <option key={index} value={marc._id}>{marc.descripcion}</option>
-                            ))
-                         }
-                    </select>
-                </div>
-                <div className="mb-3">
-                    <button type="button" class="btn btn-primary">Guardar</button>
-                </div>
-
+                <form>
+                    <div className="mb-3 col-3">
+                        <label for="patente" className="form-label">Patente</label>
+                        <input type="text" className="form-control" id="patente" name="patente" value={patente} onChange={handleInputChangePatente}></input>
+                    </div>
+                    <div className="mb-3 col-3">
+                        <label for="anio" className="form-label">Año</label>
+                        <input type="text" className="form-control" id="anio" name = "anio" value={anio} onChange={handleInputChangeAnio}></input>
+                    </div>
+                    <div className="mb-3 col-3">
+                        <label for="marca1" className="form-label">Marca</label>
+                        <select className="form-select" aria-label="Default select example" id="marca1" name="marca1" onChange={cambioSelectMarca}>
+                            <option value="0" disabled selected>Seleccione una marca...</option>
+                            {
+                            marca?.map((marc, index)=>(
+                                    <option key={index} value={marc._id}>{marc.descripcion}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    <div className="mb-3">
+                        <button type="button" className="btn btn-primary" onClick={()=>enviarDatos()}>Guardar</button>
+                    </div>
+                </form>
                 <br></br>
                 <br></br>
                 <br></br>
+                {/* Tabla de autos */}
                 <div className="mb-3 col-3">
-                <table class="table table-success table-striped">
+                <table className="table table-success table-striped">
                     <thead>
                         <tr>
                            
